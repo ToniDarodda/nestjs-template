bb035404633b92b3a06e6f25b28cd89b83751de6
Toni Da Rodda <toni.da.rodda.pro@gmail.com>
2024-08-03 12:42:49 +0200

    feat: update package.json dev deps

--- dist/strategy/jwt.strategy.d.ts
+++
@@ -1,9 +0,0 @@
-import { AccountService } from '../modules/account/service/account.service';
-import { JwtPayload } from '../types/auth';
-declare const JwtStrategy_base: new (...args: any[]) => any;
-export declare class JwtStrategy extends JwtStrategy_base {
-    private readonly accountService;
-    constructor(accountService: AccountService);
-    validate(payload: JwtPayload): Promise<import("../entities/account").Account>;
-}
-export {};
